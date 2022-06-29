b7d3b7e612f856c3427c7a5289371c7d38471481
Cebolenkosi Shezi <cbshezi5@gmail.com>
2022-06-29 18:30:06 +0200

    Update globals.ts

--- src/globals.ts
+++ src/globals.ts
@@ -1,9 +1,9 @@
 
 //REST API ENDPOINT
-export const GRAPH_HOSTNAME = "http://ec2-34-235-114-194.compute-1.amazonaws.com"
+export const GRAPH_HOSTNAME = "https://graph-orientation.herokuapp.com"
 
 //FILE TRANSFER ENDPOINT
-export const APP_HOSTNAME = "http://ec2-18-234-218-224.compute-1.amazonaws.com"
+export const APP_HOSTNAME = "https://app-orientation.herokuapp.com"
 
 //SOCKETIO ENDPOINT
 export const SOCK_HOSTNAME = "http://ec2-54-234-88-147.compute-1.amazonaws.com"
